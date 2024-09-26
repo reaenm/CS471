360e9c66ab9ecc590f3efc57c77d2f02d346fb03
reaenm <csraneem@gmail.com>
2024-09-26 04:16:40 +0300

    Lab 4 Tasks

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse  # Keep this for index2 function
 
+# Existing view for the index page
 def index(request):
     name = request.GET.get("name") or "world!"
     return render(request, "bookmodule/index.html", {"name": name})
 
+# Existing view for index2, handling integers
 def index2(request, val1=0):
     try:
         val1 = int(val1)  # Try to convert the input to an integer
         return HttpResponse(f"value1 = {val1}")
     except ValueError:
         return HttpResponse("Error, expected val1 to be an integer")  # Handle non-integer input
+
+# Existing view for viewing a single book
 def viewbook(request, bookId):
     # Simulate a list of books
     book1 = {'id': 123, 'title': 'Continuous Delivery', 'author': 'J. Humble and D. Farley'}
@@ -26,3 +30,10 @@ def viewbook(request, bookId):
     # Pass the book to the template
     context = {'book': targetBook}
     return render(request, 'bookmodule/show.html', context)
+
+# New view functions for Task 4
+def list_books(request):
+    return render(request, 'bookmodule/list_books.html')
+
+def aboutus(request):
+    return render(request, 'bookmodule/aboutus.html')
--- libraryproject/settings.py
+++ libraryproject/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'libraryproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR],  # Add TEMPLATE_DIR here
+        'DIRS': [os.path.join(BASE_DIR, 'apps/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -70,6 +70,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'libraryproject.wsgi.application'
 
@@ -120,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [(os.path.join(BASE_DIR, "apps/static"))]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
